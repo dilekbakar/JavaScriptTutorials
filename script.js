27fb96c106e8b61087ec02bd39cc6728c351a2e3
dilekbakar <dilekbakar22@gmail.com>
2023-06-02 14:39:22 +0300

    commit

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 // JavaScript source code
-// String metodlarý
+// String metodlari
 let str = 'Hello, World!';
 
 // length: Dizinin uzunluðunu verir
 let length = str.length;
 console.log('Length:', length);
 
-// substring: Belirli bir alt dizesini alýr
+// substring: Belirli bir alt dizesini alir
 let substring = str.substring(7);
 console.log('Substring:', substring);
 
-// toUpperCase: Metni büyük harflere dönüþtürür
+// toUpperCase: Metni büyük harflere dönüstürür
 let upperCase = str.toUpperCase();
 console.log('Upper Case:', upperCase);
 
-// includes: Metinde belirli bir alt dizinin varlýðýný kontrol eder
+// includes: Metinde belirli bir alt dizinin varliðini kontrol eder
 let includesWorld = str.includes('World');
 console.log('Includes "World":', includesWorld);
 
 
-// Dizi (Array) metodlarý
+// Dizi (Array) metodlari
 let numbers = [1, 2, 3, 4, 5];
 
 // push: Diziye yeni bir eleman ekler
 numbers.push(6);
 console.log('Pushed Array:', numbers);
 
-// pop: Dizinin son elemanýný çýkarýr
+// pop: Dizinin son elemanini cikarir
 let poppedElement = numbers.pop();
 console.log('Popped Element:', poppedElement);
 
-// join: Diziyi birleþtirerek string haline getirir
+// join: Diziyi birlestirerek string haline getirir
 let joinedArray = numbers.join(',');
 console.log('Joined Array:', joinedArray);
 
-// forEach: Dizi elemanlarý üzerinde döngü iþlemi yapar
+// forEach: Dizi elemanlari üzerinde döngü islemi yapar
 numbers.forEach(function (number) {
     console.log('Number:', number);
 });
@@ -43,30 +43,30 @@ numbers.forEach(function (number) {
 // Matematiksel metodlar
 let num = 4.7;
 
-// Math.floor: Sayýyý aþaðýya yuvarlar
+// Math.floor: Sayiyi asaðiya yuvarlar
 let floorValue = Math.floor(num);
 console.log('Floor:', floorValue);
 
-// Math.ceil: Sayýyý yukarýya yuvarlar
+// Math.ceil: Sayiyi yukariya yuvarlar
 let ceilValue = Math.ceil(num);
 console.log('Ceil:', ceilValue);
 
-// Math.random: 0 ile 1 arasýnda rastgele sayý üretir
+// Math.random: 0 ile 1 arasinda rastgele sayi üretir
 let randomValue = Math.random();
 console.log('Random:', randomValue);
 
 
-// Zaman metodlarý
-// Date.now: Þu anki zamaný milisaniye cinsinden verir
+// Zaman metodlari
+// Date.now: su anki zamani milisaniye cinsinden verir
 let currentTime = Date.now();
 console.log('Current Time:', currentTime);
 
-// setTimeout: Belirli bir süre sonra bir fonksiyonu çalýþtýrýr
+// setTimeout: Belirli bir süre sonra bir fonksiyonu calistirir
 setTimeout(function () {
     console.log('Delayed function');
 }, 2000);
 
-// setInterval: Belirli aralýklarla bir fonksiyonu tekrar tekrar çalýþtýrýr
+// setInterval: Belirli araliklarla bir fonksiyonu tekrar tekrar calistirir
 setInterval(function () {
     console.log('Interval function');
 }, 1000);
